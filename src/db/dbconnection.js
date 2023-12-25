0a1f4e6b1883f368d97d907a4ff316a33ce32b46
Rakesh <21se02cs014@ppsu.ac.in>
2023-12-25 21:24:59 +0530

    minor changes

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n MongoDB connection SUCCESS.!! | DB Host : ${connectionInstance.connection.Host}`);
+    console.log(`\n MongoDB connection SUCCESS.!! | DB Host : ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection FAILED", error);
     process.exit(1);
