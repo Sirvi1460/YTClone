0b2064d4cba661975f03343ac592289a3e501b52
Rakesh <21se02cs014@ppsu.ac.in>
2023-12-28 15:23:15 +0530

    setting up Utils code.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbconnection.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+  try {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is Running at Port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    app.listen("error", (error) => {
+        console.log("Error :", error);
+        throw error
+    })
+  }  
+})
+.catch((error) => {
+    console.log("Mongo DB Connection Failed !!", error)
+})
+
+
+// here in .then() : Try-Catch is applied by me so try default code as shown in video while Serious error.
